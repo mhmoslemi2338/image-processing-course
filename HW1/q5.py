111c25c7274f08613f1547c65772509ae1d78ccb
Mohammad hosein moslemi <mhmoslemi2338@.com>
2021-10-21 14:43:08 +0330

    some minor fixes

--- HW1/q5.py
+++ HW1/q5.py
@@ -2,30 +2,64 @@ import cv2
 import numpy as np
 import timeit
 
-#### method one
-img=cv2.imread('Pink.jpg')
-kernel=1/9*np.ones((3,3))
+
+def shift(image, tx, ty):
+    (h, w, _) = np.shape(image)
+    T = np.array([[1, 0, tx],
+                  [0, 1, ty]], dtype=np.float32)
+    image = cv2.warpAffine(image, T, (w, h))
+    return image
+
+
+############ method one ####################
+img = cv2.imread('Pink.jpg')
 start = timeit.default_timer()
 
-img2 = cv2.filter2D(src=img, kernel=kernel, ddepth=-1)
+img2 = cv2.boxFilter(img, ksize=(3, 3), normalize=True, ddepth=-1)
+res = img2[1:-1, 1:-1, :]
 
 stop = timeit.default_timer()
-print('Run-Time for method one : %0.4f sec'%(stop - start))
-cv2.imwrite('res07.jpg',img2)
+print('Run-Time for method one : %0.4f sec' % (stop - start))
+cv2.imwrite('res07.jpg', res)
 
-#### method two
-img=cv2.imread('Pink.jpg')
-img2=img.copy()
-(h,w,_)=np.shape(img2)
 
-for i in range(h-2):
-    for j in range(w-2):
-        img2[i+1,j+1,0]=int(np.sum(img[i:i+3,j:j+3,0])/9)
-        img2[i+1,j+1,1]=int(np.sum(img[i:i+3,j:j+3,1])/9)
-        img2[i+1,j+1,2]=int(np.sum(img[i:i+3,j:j+3,2])/9)
-        
-res=np.uint8(img2)
+############ method two ####################
+img = cv2.imread('Pink.jpg')
+img2 = img.copy()
+start = timeit.default_timer()
+
+(h, w, _) = np.shape(img2)
+for i in range(h - 2):
+    for j in range(w - 2):
+        img2[i + 1, j + 1, 0] = \
+            np.uint8(np.float64(np.sum(img[i:i + 3, j:j + 3, 0])) / 9)
+        img2[i + 1, j + 1, 1] = \
+            np.uint8(np.float64(np.sum(img[i:i + 3, j:j + 3, 1])) / 9)
+        img2[i + 1, j + 1, 2] = \
+            np.uint8(np.float64(np.sum(img[i:i + 3, j:j + 3, 2])) / 9)
+res = img2[1:-1, 1:-1, :]
+
 stop = timeit.default_timer()
-print('Run-Time for method two : %0.3f sec' %(stop - start))
-cv2.imwrite('res08.jpg',img2)
+print('Run-Time for method two : %0.3f sec' % (stop - start))
+cv2.imwrite('res08.jpg', res)
 
+
+############ method Three ####################
+img = cv2.imread('Pink.jpg')
+start = timeit.default_timer()
+
+img2 = np.float64(img.copy())  # base
+img2 += np.float64(shift(img, 1, 0))  # left
+img2 += np.float64(shift(img, -1, 0))  # right
+img2 += np.float64(shift(img, 0, -1))  # up
+img2 += np.float64(shift(img, 0, 1))  # down
+img2 += np.float64(shift(img, 1, 1))  # down + left
+img2 += np.float64(shift(img, -1, 1))  # down + right
+img2 += np.float64(shift(img, 1, -1))  # up + left
+img2 += np.float64(shift(img, -1, -1))  # up + right
+img2 = np.uint8((img2) / 9)
+res = img2[1:-1, 1:-1, :]
+
+stop = timeit.default_timer()
+print('Run-Time for method Three : %0.3f sec' % (stop - start))
+cv2.imwrite('res09.jpg', res)
